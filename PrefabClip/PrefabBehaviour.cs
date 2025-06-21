7f25ec389c2e2128874beb696c97cba75e2bd7ce
ayaha401 <75297336+ayaha401@users.noreply.github.com>
2025-06-21 12:13:12 +0900

    Create PrefabBehaviour.cs

---
+++ PrefabClip/PrefabBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PrefabBehaviour : PlayableBehaviour
+{
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+    }
+}
